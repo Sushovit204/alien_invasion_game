ab2db6bf029917b8540947ffcf32718132d82685
Sushovit204 <shahsushovit434@gmail.com>
2022-08-15 14:02:39 +0545

    Screen has been set up

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+# the tools in sys module can be used to quit the game when the player wants
 
 import pygame
 
@@ -10,21 +11,23 @@ class AlienInvasion:
         """Initialize the game and create resources."""
         pygame.init()
 
+        # game window which will be 1200 pixel wide and 800 pixel height
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion By @Sushovit204")
 
     def run_game(self):
         """Start the main loop for the game."""
-    while True:
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
-    if __name__ == "__main__":
-        # Make a game instance and run the game.
-        ai = AlienInvasion()
-        ai.run_game()
+
+if __name__ == '__main__':
+    # Make a game instance and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
