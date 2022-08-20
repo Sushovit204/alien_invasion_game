00abb2d9749d3d95d6de60fc39c113c32a0a16f4
Sushovit204 <shahsushovit434@gmail.com>
2022-08-20 21:48:57 +0545

    Horizontal speed still not adjusted

--- settings.py
+++ settings.py
@@ -4,19 +4,19 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings"""
         # Screen Settings
-        self.screen_width = 1180
+        self.screen_width = 2000
         self.screen_height = 800
         self.bg_color = (118, 118, 118)
         # ship setting
         self.ship_speed = 1.5
         # Bullet setings
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 8
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
         # Alien Settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 1
+        self.fleet_drop_speed = 0
         # fleet_direction of 1 represents right and -1 represents left
         self.fleet_direction = 1
