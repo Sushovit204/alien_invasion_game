103054741db2ed29fab6085d94a06b08f81b10da
Sushovit204 <shahsushovit434@gmail.com>
2022-08-20 18:19:30 +0545

    Made a full fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,7 +87,7 @@ class AlienInvasion:
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
-        number_alien_x = available_space_x // (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
 
         # Determine the no. of rows of alien that fit on the screen
         ship_height = self.ship.rect.height
@@ -96,18 +96,17 @@ class AlienInvasion:
         number_rows = available_space_y // (2*alien_height)
 
         # Create full fleets of aliens
-
-        # Create first row of aliens
         for row_number in range(number_rows):
-            for alien_number in range(number_alien_x):
+            for alien_number in range(number_aliens_x):
                 self._create_alien(alien_number, row_number)
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         # Create an alien and place it in the row
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
